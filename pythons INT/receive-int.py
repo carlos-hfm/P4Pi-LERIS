eaa4362a753c64fb0ed18a4102174f3ff24e83d3
Carlos de França <carloshfm@hotmail.com>
2023-08-17 22:57:13 -0300

    acessando campos do header INT manualmente

--- pythons INT/receive-int.py
+++ pythons INT/receive-int.py
@@ -6,43 +6,54 @@ import random
 import struct
 
 from time import sleep
-from scapy.all import Packet, bind_layers, XByteField, FieldLenField, BitField, ShortField, IntField, PacketListField, \
-    Ether, IP, UDP, sendp, get_if_hwaddr, sniff
-
+from scapy.all import Packet, bind_layers, BitField, ShortField, IntField, Ether, IP, UDP, sendp, get_if_hwaddr, sniff, PacketListField
 
 class InBandNetworkTelemetry(Packet):
-    fields_desc = [BitField("switchID_t", 0, 31),
-                   BitField("ingress_port", 0, 9),
-                   BitField("egress_port", 0, 9),
-                   BitField("egress_spec", 0, 9),
-                   BitField("ingress_global_timestamp", 0, 48),
-                   BitField("egress_global_timestamp", 0, 48),
-                   BitField("enq_timestamp", 0, 32),
-                   BitField("enq_qdepth", 0, 19),
-                   BitField("deq_timedelta", 0, 32),
-                   BitField("deq_qdepth", 0, 19)
-                   ]
+    fields_desc = [
+        BitField("switchID_t", 0, 31),
+        BitField("ingress_port", 0, 9),
+        BitField("egress_port", 0, 9),
+        BitField("egress_spec", 0, 9),
+        BitField("ingress_global_timestamp", 0, 48),
+        BitField("egress_global_timestamp", 0, 48),
+        BitField("enq_timestamp", 0, 32),
+        BitField("enq_qdepth", 0, 19),
+        BitField("deq_timedelta", 0, 32),
+        BitField("deq_qdepth", 0, 19)
+    ]
     """any thing after this packet is extracted is padding"""
 
     def extract_padding(self, p):
         return "", p
 
-
-class nodeCount(Packet):
+class NodeCount(Packet):
     name = "nodeCount"
     fields_desc = [ShortField("count", 0),
                    PacketListField("INT", [], InBandNetworkTelemetry, count_from=lambda pkt: (pkt.count * 1))]
 
-
 def handle_pkt(pkt):
-    pkt.show2()
-
-
+    #pkt.show2()
+    if NodeCount in pkt:
+        for int_pkt in pkt[NodeCount].INT:
+            telemetry = int_pkt[InBandNetworkTelemetry]
+            print("Switch ID:", telemetry.switchID_t)
+            print("Ingress Port:", telemetry.ingress_port)
+            print("Egress Port:", telemetry.egress_port)
+            print("Egress Spec:", telemetry.egress_spec)
+            print("Ingress Global Timestamp:", telemetry.ingress_global_timestamp)
+            print("Egress Global Timestamp:", telemetry.egress_global_timestamp)
+            print("Enqueue Timestamp:", telemetry.enq_timestamp)
+            print("Enqueue Queue Depth:", telemetry.enq_qdepth)
+            print("Dequeue Timedelta:", telemetry.deq_timedelta)
+            print("Dequeue Queue Depth:", telemetry.deq_qdepth)
+            print("------------------------------")
 def main():
     iface = 'Wi-Fi' #interface de entrada, alterar para Ethernet quando necessário
-    bind_layers(IP, nodeCount, proto=253)
-    sniff(filter="ip proto 253", iface=iface, prn=lambda x: handle_pkt(x))
 
+    bind_layers(IP, NodeCount, proto=253)  
+    bind_layers(Ether, IP)
+
+    sniff(filter="ip proto 253", iface=iface, prn=lambda x: handle_pkt(x))
 
 if __name__ == '__main__':
     main()
