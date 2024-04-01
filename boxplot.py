9a27792b8fb1edb7d0e7bfababdb34feaca443e4
Carlos de França <carloshfm@hotmail.com>
2024-04-01 18:38:21 -0300

    add py para criar boxplot

---
+++ boxplot.py
@@ -0,0 +1,6 @@
+'''
+    criar boxplots dos dados INT
+        - 1 plotar um grafico só
+        - plotar varios
+        - salvar valores relevantes (media, mediana, max, min, Q1, Q3, percentil 95, limites superior e inferior)
+'''
